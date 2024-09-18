264afefed97711e613c4551073fe9b92d2958559
Nitiya05 <nitiyarihaisy@gmail.com>
2024-09-18 09:23:02 +0700

    pert4

---
+++ pertemuan 4/kodeJs.js
@@ -0,0 +1,5 @@
+// untuk menampilkan jendela dialog
+//alert("Hello, ini adalah program JS eksternal!");
+
+//untuk menulis sesuatu ke dokumen HTML.
+// document.write("Kode Javascript dari File Eksternal");
